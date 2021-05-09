c7851c3556d59d11cd25019f53ffaf7a8d313349
hakkeiy <38604374+hakkeiy@users.noreply.github.com>
2021-05-09 20:47:42 +0900

    doit

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,15 @@ import { useState } from "react";
 import axios from "axios";
 import "./styles.css";
 import { Todo } from "./todo";
-
-type todoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import { TodoType } from "./types/todo";
+import { Text } from "./text";
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<todoType>>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
     axios
-      .get<Array<todoType>>("https://jsonplaceholder.typicode.com/todos")
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => {
         setTodos(res.data);
       });
@@ -23,9 +18,15 @@ export default function App() {
 
   return (
     <div className="App">
-      <button onClick={onClickFetchData}>ボタン</button>
+      <Text color="red" fontSize="18px" />
+      <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userId={todo.userId} completed={todo.userId} />
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
